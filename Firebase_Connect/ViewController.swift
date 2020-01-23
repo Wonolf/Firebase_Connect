13beb57b240afc9a378115f913df0b6d492adc27
Bonoso Sierra <bonoso96@outlook.es>
2020-01-23 16:26:53 +0100

    Initial Commit

---
+++ Firebase_Connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase_Connect
+//
+//  Created by Bononya on 23/01/2020.
+//  Copyright © 2020 Bononya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
